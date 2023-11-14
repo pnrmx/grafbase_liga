885ac65db8567dfb8af30efd5abae6c7b09354c4
Pavel Kovalenko <unixnews@mail.ru>
2023-11-14 19:50:13 +0400

    first commit

---
+++ grafbase.config.ts
@@ -0,0 +1,43 @@
+import { g, config } from '@grafbase/sdk'
+
+const User = g.model('User',{
+  name: g.string().length({ min: 2, max: 20}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User ),
+})
+
+export default config({
+  schema: g,
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public()
+    },
+  },
+  // Caching - https://grafbase.com/docs/graphql-edge-caching
+  // cache: {
+  //   rules: [
+  //     {
+  //       types: ['Query'], // Cache everything for 60 seconds
+  //       maxAge: 60,
+  //       staleWhileRevalidate: 60
+  //     }
+  //   ]
+  // }
+})
